6f6afef474e44042ddf7a7cfb16c91cf365dda70
Ricardo Carvalho <ricardo.carvalho@xolyd.com>
2025-06-14 16:51:35 +0100

    Added next.config.js with images.unoptimized=true to support <Image /> in static export.

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export',
+  images: {
+    unoptimized: true, // Disables Image Optimization API for static exports
+  },  
 };
 
 export default nextConfig;
